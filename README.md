47e2bfdb2b20e0ec9de82b9d841c164f79471ee5
avelarder-kz <arturo.velarde@kzsoftworks.com>
2021-06-25 22:13:08 -0300

    Initial changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KzRC-Onboarding
+
 Basic web app using Three.js and Blender to model 3d object. It was fun and definitely could be better, do you want to contribute? Go for it.
+
+```
+npm install
+npm run dev
+```
---
+++ main.js
@@ -0,0 +1,71 @@
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const gLTFLoader = new GLTFLoader();
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+  alpha: true,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0x5103a3);
+scene.add(pointLight, ambientLight);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.08, 25, 25);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    blending: THREE.NormalBlending,
+  });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+gLTFLoader.load("SpaceShuttel.gltf", (gltf) => {
+  gltf.scene.rotation.x = 1092;
+  gltf.scene.rotation.y = 478;
+
+  scene.add(gltf.scene);
+});
+
+Array(1500).fill().forEach(addStar);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.02;
+}
+document.body.onscroll = moveCamera;
+
+moveCamera();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
