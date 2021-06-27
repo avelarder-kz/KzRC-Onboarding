29db1f8d718991972b4cc780537bfed85f69df09
arturo.velarde <arturo.velarde@kzsoftworks.com>
2021-06-27 01:20:29 -0300

    taking off

--- main.js
+++ main.js
@@ -29,6 +29,17 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0x5103a3);
 scene.add(pointLight, ambientLight);
 
+var pointLight2 = new THREE.PointLight("0xffffff");
+pointLight2.position.set(-1.7, 0, 0);
+pointLight2.add(
+  new THREE.Mesh(
+    new THREE.SphereGeometry(1, 10, 10),
+    new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+    })
+  )
+);
+
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.08, 25, 25);
   const material = new THREE.MeshStandardMaterial({
@@ -45,11 +56,14 @@ function addStar() {
   scene.add(star);
 }
 
+let gltfReference = undefined;
+
 gLTFLoader.load("SpaceShuttel.gltf", (gltf) => {
   gltf.scene.rotation.x = 1092;
   gltf.scene.rotation.y = 478;
 
-  scene.add(gltf.scene);
+  gltfReference = gltf.scene;
+  scene.add(gltfReference);
 });
 
 Array(1500).fill().forEach(addStar);
@@ -59,12 +73,35 @@ function moveCamera() {
 
   camera.position.z = t * -0.01;
 }
+
+let takeoff = false;
+
+function takeOff() {
+  scene.add(pointLight2);
+  takeoff = true;
+}
+
 document.body.onscroll = moveCamera;
+document.querySelector("#btnTakeOff").onclick = takeOff;
 
 moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
+
+  if (takeoff) {
+    gltfReference.position.z -= 10;
+    pointLight2.position.z += 1;
+    pointLight2.position.x -= 0.04;
+    pointLight2.scale.x += 2.5;
+    pointLight2.scale.y += 0.2;
+    pointLight2.scale.z += 0.2;
+
+    if (pointLight2.position.z > camera.position.z) {
+      takeoff = false;
+    }
+  }
+
   renderer.render(scene, camera);
 }
 
